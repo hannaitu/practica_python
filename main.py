46124c7289e0674cfc4a4c488c1ee6fdf676468b
Hanna Iturralde <hannaitu2004@gmail.com>
2026-03-19 00:09:43 -0300

    Ejercicio 1

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-nombre = "Juan"
-apellido = "Castaño"
+año_nacimiento = int(input("Ingrese año de nacimiento 200"))
 
-print(f"Mi nombre completo es {nombre} {apellido}")
-print(f"prueba sincronizacion")
+años_18 = año_nacimiento + 18
+años_21 = año_nacimiento + 21
+años_100 = año_nacimiento + 100
+
+print(f"Cumple 18 años en: {años_18}")
+print(f"Cumple 21 años en: {años_21}")
+print(f"Cumple 100 años en: {años_100}")
